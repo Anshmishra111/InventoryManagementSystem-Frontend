20c597616eef74c0e25780868f673c106d20e360
Himanshu Mishra <anshmishra111@gmail.com>
2026-05-16 11:33:37 +0530

    Update auth interceptor

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,19 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth';
+import { environment } from '../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
 
+  let apiReq = req;
+  
+  // If the request is to our /api, prepend the full base URL from environment
+  if (req.url.startsWith('/api')) {
+    apiReq = req.clone({
+      url: `${environment.apiUrl}${req.url.replace('/api', '')}`
+    });
+  }
+
   if (token) {
-    const authReq = req.clone({
+    apiReq = apiReq.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(authReq);
   }
 
-  return next(req);
+  return next(apiReq);
 };
